f43e1a33bf50668349daeabe0e1f8f7514033a0b
PRADEEP999901 <149851567+PRADEEP999901@users.noreply.github.com>
2024-12-30 10:13:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("23435751"))
+API_HASH = getenv("2345de86dfb7ec6ef52ee0c16dd11dea")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
